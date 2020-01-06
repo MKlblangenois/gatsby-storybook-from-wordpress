1b04d9aece72689ee6db92eb47c06251251d8f44
Logan <logan.blangenois@mediakod.com>
2020-01-06 14:51:05 +0100

    add support for sass in Storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,25 +1,46 @@
-module.exports = ({ config }) => {
-  // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
+// Based on https://github.com/m-allanson/gatsby-storybook-css-modules
+// and adapted to work with SASS.
+require("imports-loader");
 
-  // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
-  config.module.rules[0].use[0].loader = require.resolve("babel-loader");
+const path = require("path");
 
-  // use @babel/preset-react for JSX and env (instead of staged presets)
-  config.module.rules[0].use[0].options.presets = [
-    require.resolve("@babel/preset-react"),
-    require.resolve("@babel/preset-env"),
-  ];
+module.exports = ({config}) => {
 
-  config.module.rules[0].use[0].options.plugins = [
-    // use @babel/plugin-proposal-class-properties for class arrow functions
-    require.resolve("@babel/plugin-proposal-class-properties"),
-    // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-    require.resolve("babel-plugin-remove-graphql-queries"),
-  ];
+  // Add specific loader rule for CSS (SASS) Modules
+  config.module.rules.push({
+    test: /\.module\.s[ac]ss$/,
+    use: [
+      { loader: 'style-loader' },
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          importLoaders: 2,
+          localIdentName: "[path]-[local]-[hash:base64:5]"
+        }
+      },
+      { loader: 'postcss-loader' },
+      { loader: 'sass-loader' }
+    ],
+    include: path.resolve(__dirname, "../src")
+  });
 
-  // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
-  config.resolve.mainFields = ["browser", "module", "main"];
+  config.module.rules.push({
+    test: /\.s[ac]ss$/,
+    loaders: [
+      "style-loader",
+      "css-loader",
+      "sass-loader",
+      ],
+    exclude: /\.module\.s[ac]ss$/,
+    include: path.resolve(__dirname, '../src')
+  })
 
-  return config
+  // ignore gatsby-link's global `__loader` variable
+  config.module.rules.push({
+    test: require.resolve("gatsby-link"),
+    loaders: ["imports-loader?___loader=>{enqueue:function(){}}"]
+  });
+
+  return config;
 };
